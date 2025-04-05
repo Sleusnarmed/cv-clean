341356a7bd3aaf4e0fd865087417609323305294
José Salazar <jmsalazar.macg@hotmail.com>
2025-04-05 15:03:51 -0600

    So bad cvcreado

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Registro from './pages/auth/RegisterForm.jsx';
 import Chat from './pages/cv-creation/Chat.jsx';
 import Plantillas from './pages/cv-creation/Plantillas.jsx';
 import Pdf from './pages/cv-creation/PDF.jsx'; // Asegúrate de que este componente exista
-import CvCreado from './pages/cv-creation/CvCreado.jsx'
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
         <Route path="/registro" element={<Registro />} /> 
         <Route path="/chat" element={<Chat />} /> 
         <Route path="/plantillas" element={<Plantillas />} /> 
-        <Route path="/cvcreado/:templateId" element={<CvCreado />} />
         <Route path="/pdf" element={<Pdf />} />
 
         {/* Puedes agregar más rutas aquí según sea necesario */}
